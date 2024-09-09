7ad7b73f0b1c9f7560cb24fb7030c37f9d7759a9
dlskl <skyandme77@gmail.com>
2024-09-09 20:57:18 +0900

    feat: memoa post

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,63 +1,86 @@
 import React from "react";
 import "./index.css";
+import { MdAccountCircle, MdOutlineComment, MdBookmarkBorder} from "react-icons/md";
+import { FaRegHeart } from "react-icons/fa";
+
 
 const Post =()=>{
-    const titleList=[
+    const titleList=
         {
             id:1,
             name:"지존진교",
-            profileImg:"",
-            title:"국어 고1 천재 노트필기 공유",
+            profileImg: "src/assets/profile-img.png",
+            title:"국어 고1 천재 1-2 노트필기 공유합니다",
             day:"2024.08.33",
-            mainImg:"",
-            commentCount: "999+",
-            heartCount: "999+",
+            mainImg:"src/assets/main-img.png",
+            commentCount: 999,
+            heartCount: 999,
             isBookmark: false,
             subTag:"국어",
             rankTag:"고등",
             gradeTag:"1학년"
-        },
-    ]
+        }
+
     const clickBookmark =()=>{
-        let bookmark = titleList[0].isBookmark
+        let bookmark = titleList.isBookmark
         if (bookmark == false){
-            titleList[0].isBookmark = true
+            titleList.isBookmark = true
             
         }
         else if (bookmark == true){
-            titleList[0].isBookmark = false
+            titleList.isBookmark = false
         }
-        console.log(titleList[0].isBookmark)
+        console.log(titleList.isBookmark)
     }
     // const subjectFilter=["국어", "사회", "수학","영어","과학","한국사"]
     // const gradeFilter = ["1학년", "2학년", "3학년", "4학년", "5학년", "6학년"]
     // const rankFilter=["초등", "중등", "고등"]
     
+    // if (titleList.heartCount>=999){
+    //     titleList.heartCount = "999+"
+    // }
+    // if (titleList.commentCount>=999){
+    //     titleList.commentCount = "999+"
+    // }
+
     return(
         <>
-            <div className="postHead">
-                <img src={titleList[0].profileImg}/>
-                <div>{titleList[0].name}</div>
-                <span>{titleList[0].day}</span>
+        <div className="post-container">
+            <div className="post-head">
+                <img src={titleList.profileImg}/>
+                <div className="user-name">{titleList.name}</div>
+                <div className="post-dot">•</div>
+                <span>{titleList.day}</span>
             </div>
-            <div className="mainImg">
-                <img src={titleList[0].mainImg}/>
+            <div className="main-img">
+                <img src={titleList.mainImg}/>
             </div>
-            <div className="board">
-                <div className="tag">
-                    <div>{titleList[0].subTag}</div>
-                    <div>{titleList[0].rankTag}</div>
-                    <div>{titleList[0].gradeTag}</div>
-                </div>
-                <div className="postTitle">{titleList[0].title}</div>
+            <div className="post-footer">
                 <div className="icons">
-                    <div className="selectOne">
-                        <span className="comment">comment</span>
-                        <span className="heart">heart</span>
+                    <div className="post-state">
+                        <div className="post-comment">
+                            <MdOutlineComment style={{width:'24px', height:'24px'}}/>
+                            <span>{titleList.commentCount >= 999 ? '999+' : titleList.commentCount}</span>
+                        </div>
+                        <div className="post-heart">
+                            <FaRegHeart style={{width:'24px', height:'24px'}}/>
+                            <span>{titleList.heartCount >= 999 ? '999+' : titleList.heartCount}</span>
+                        </div>
                     </div>
-                    <div className="selectTwo" onClick={clickBookmark}>bookmark</div>
+                    <div className="post-state-two" onClick={clickBookmark}>
+                        <MdBookmarkBorder style={{width:'28px', height:'28px'}}/>
+                    </div>
+                </div>
+            <div className="post-board">
+                <div className="post-tag">
+                    <div>{titleList.subTag}</div>
+                    <div>{titleList.rankTag}</div>
+                    <div>{titleList.gradeTag}</div>
                 </div>
             </div>
+            <div className="post-title">{titleList.title}</div>
+        </div>
+        </div>
         </>
     )
 
--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,7 +1,7 @@
 
 import Header from "../../components/Header/Header"
 import Sidebar from "../../components/Sidebar/Sidebar"
-
+import Post from "../../components/Post/post"
 const Profile = () => {
   return (
     <div className="layout">
