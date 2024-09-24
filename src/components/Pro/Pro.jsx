f209751e28530357a611f35e68dabbf8ccb0da4d
dlskl <skyandme77@gmail.com>
2024-09-24 20:36:29 +0900

    feat : user data

--- src/components/Pro/Pro.jsx
+++ src/components/Pro/Pro.jsx
@@ -1,33 +1,56 @@
 import React from "react";
 import "./index.css";
+import { useParams } from "react-router-dom";
 
-const Pro=()=>{
-    const userInfo={
-        name:"지존진교",
-        profileImg: "src/assets/profile-img.png",
-        introduce:"나는지존짱짱",
-        postCount : 12,
-        followerCount: 123,
-        followCount:234
-    }
-    return(
-        <div className="profile-container">
-            <div className="user-pro-img">
-                <img src={userInfo.profileImg} style={{width:"160px", height:"160px"}} />
-            </div>
-            <div className="user-info-container">
-                <div className="user-introduce">
-                    <div>{userInfo.name}</div>
-                    <span>{userInfo.introduce}</span>
-                </div>
-                    <div className="user-information">
-                            <div>작성한 글 <span className="user-number">{userInfo.postCount}</span></div>
-                            <div>팔로워 <span className="user-number">{userInfo.followerCount}</span></div>
-                            <div>팔로우 <span className="user-number">{userInfo.followCount}</span></div>
-                        </div>
-                    </div>  
-            </div>
-    )
-}
+const Pro = () => {
+  const userInfo = [
+    {
+      name: "지존진교",
+      profileImg: "../src/assets/profile-img.png",
+      introduce: "나는지존짱짱",
+      postCount: 12,
+      followerCount: 123,
+      followCount: 234,
+    },
+    {
+      name: "cugar",
+      profileImg: "../src/assets/profile-img.png",
+      introduce: "나는여승원뚠뚠",
+      postCount: 12,
+      followerCount: 123,
+      followCount: 234,
+    },
+  ];
 
-export default Pro
+  const index = 0;
+
+  return (
+    <div className="profile-container">
+      <div className="user-pro-img">
+        <img
+          src={userInfo[index].profileImg}
+          style={{ width: "160px", height: "160px" }}
+        />
+      </div>
+      <div className="user-info-container">
+        <div className="user-introduce">
+          <div>{userInfo[index].name}</div>
+          <span>{userInfo[index].introduce}</span>
+        </div>
+        <div className="user-information">
+          <div>
+            작성한 글 <span className="user-number">{userInfo[index].postCount}</span>
+          </div>
+          <div>
+            팔로워 <span className="user-number">{userInfo[index].followerCount}</span>
+          </div>
+          <div>
+            팔로우 <span className="user-number">{userInfo[index].followCount}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Pro;
--- src/components/router/index.jsx
+++ src/components/router/index.jsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Login from '../../pages/Login/Login'
-import Signup from '../../pages/Signup/Signup'
-import Home from '../../pages/Home/Home'
-import Follow from '../../pages/Follow/Follow'
-import Bookmark from '../../pages/Bookmark/Bookmark'
-import Profile from '../../pages/Profile/Profile'
-import School from '../../pages/School/School'
-import Setting from '../../pages/Setting/Setting'
-import Search from '../../pages/Search/Search'
-import Help from '../../pages/Help/Help'
-import Sidebar from '../Sidebar/Sidebar'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "../../pages/Login/Login";
+import Signup from "../../pages/Signup/Signup";
+import Home from "../../pages/Home/Home";
+import Follow from "../../pages/Follow/Follow";
+import Bookmark from "../../pages/Bookmark/Bookmark";
+import Profile from "../../pages/Profile/Profile";
+import School from "../../pages/School/School";
+import Setting from "../../pages/Setting/Setting";
+import Search from "../../pages/Search/Search";
+import Help from "../../pages/Help/Help";
+import Sidebar from "../Sidebar/Sidebar";
 
 const Router = () => {
   return (
@@ -17,21 +17,21 @@ const Router = () => {
       <div className="layout">
         <Sidebar />
         <Routes>
-          <Route path='/login' element={<Login />} />
-          <Route path='/signup' element={<Signup/>}/>
-          <Route path='/home' element={<Home />} />
-          <Route path='/follow' element={<Follow />}/>
-          <Route path='/bookmark' element={<Bookmark />}/>
-          <Route path='/profile' element={<Profile />}/>
-          <Route path='/school' element={<School />}/>
-          <Route path='/setting' element={<Setting />}/>
-          <Route path='/search' element={<Search />}/>
-          <Route path='/help' element={<Help/>}/>
-          <Route path='/sidebar' element={<Sidebar/>}/>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/follow" element={<Follow />} />
+          <Route path="/bookmark" element={<Bookmark />} />
+          <Route path="/profile/:username" element={<Profile />} />
+          <Route path="/school" element={<School />} />
+          <Route path="/setting" element={<Setting />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/help" element={<Help />} />
+          <Route path="/sidebar" element={<Sidebar />} />
         </Routes>
       </div>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default Router
+export default Router;
