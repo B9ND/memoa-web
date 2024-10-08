aa26b5fd51e2d99306783bc61f18658c5bd87873
dlskl <skyandme77@gmail.com>
2024-10-08 19:29:55 +0900

    feat : update profile

--- src/components/Pro/Pro.jsx
+++ src/components/Pro/Pro.jsx
@@ -1,51 +1,86 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./index.css";
-import { useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
+// import { useLocation } from "react-router-dom";
+
+const userInfo = [
+  {
+    name: "지존진교",
+    profileImg: "../src/assets/profile-img.png",
+    introduce: "나는지존짱짱",
+    postCount: 12,
+    followerCount: 123,
+    followCount: 234,
+  },
+  {
+    name: "cugar",
+    profileImg: "../src/assets/profile-img.png",
+    introduce: "나는여승원뚠뚠",
+    postCount: 12,
+    followerCount: 123,
+    followCount: 234,
+  },
+];
 
 const Pro = () => {
-  const userInfo = [
-    {
-      name: "지존진교",
-      profileImg: "../src/assets/profile-img.png",
-      introduce: "나는지존짱짱",
-      postCount: 12,
-      followerCount: 123,
-      followCount: 234,
-    },
-    {
-      name: "cugar",
-      profileImg: "../src/assets/profile-img.png",
-      introduce: "나는여승원뚠뚠",
-      postCount: 12,
-      followerCount: 123,
-      followCount: 234,
-    },
-  ];
+  const { username } = useParams();
+  const UserName = username.replace(":", "");
+  let userIndex = userInfo.findIndex((user) => user.name === UserName);
 
-  const index = 0;
+  console.log(userIndex);
+  console.log(UserName);
+
+  const location = useLocation();
+  console.log(location)
+  
+  
+
+  if (userIndex === -1) {
+    return <div>This is not user</div>;
+  }
+
+  let isVisible = true;
+  if (UserName === userInfo[0].name) {
+    isVisible = false;
+  }
 
   return (
     <div className="profile-container">
       <div className="user-pro-img">
         <img
-          src={userInfo[index].profileImg}
+          src={userInfo[userIndex].profileImg}
           style={{ width: "160px", height: "160px" }}
         />
       </div>
       <div className="user-info-container">
         <div className="user-introduce">
-          <div>{userInfo[index].name}</div>
-          <span>{userInfo[index].introduce}</span>
+          <div>
+            <div>{userInfo[userIndex].name}</div>
+            <button
+              className="follow"
+              style={{ display: isVisible ? "block" : "none" }}
+            >
+              팔로잉
+            </button>
+          </div>
+          <span>{userInfo[userIndex].introduce}</span>
         </div>
         <div className="user-information">
           <div>
-            작성한 글 <span className="user-number">{userInfo[index].postCount}</span>
+            작성한 글{" "}
+            <span className="user-number">{userInfo[userIndex].postCount}</span>
           </div>
           <div>
-            팔로워 <span className="user-number">{userInfo[index].followerCount}</span>
+            팔로워{" "}
+            <span className="user-number">
+              {userInfo[userIndex].followerCount}
+            </span>
           </div>
           <div>
-            팔로우 <span className="user-number">{userInfo[index].followCount}</span>
+            팔로우{" "}
+            <span className="user-number">
+              {userInfo[userIndex].followCount}
+            </span>
           </div>
         </div>
       </div>
--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,16 +1,16 @@
-
-import Header from "../../components/Header/Header"
-import Post from "../../components/Post/post"
-import Pro from "../../components/Pro/Pro"
+import { useLocation } from "react-router-dom";
+import Header from "../../components/Header/Header";
+import Post from "../../components/Post/post";
+import Pro from "../../components/Pro/Pro";
 
 const Profile = () => {
   return (
-      <div className="head-main">
-        <Header/>
-        <Pro/>
-        <Post/>
-      </div>
-  )
-}
+    <div className="head-main">
+      <Header />
+      <Pro />
+      <Post />
+    </div>
+  );
+};
 
-export default Profile
+export default Profile;
