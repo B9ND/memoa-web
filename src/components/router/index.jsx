1e0261bd3676fdf8c2d1de2d47c88db43aa78a8d
dlskl <skyandme77@gmail.com>
2024-10-18 23:06:14 +0900

    feat : write post 초기 셋팅

--- src/components/router/index.jsx
+++ src/components/router/index.jsx
@@ -1,16 +1,17 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Login from '../../pages/Login/Login'
-import Signup from '../../pages/Signup/Signup'
-import Home from '../../pages/Home/Home'
-import Follow from '../../pages/Follow/Follow'
-import Bookmark from '../../pages/Bookmark/Bookmark'
-import Profile from '../../pages/Profile/Profile'
-import School from '../../pages/School/School'
-import Setting from '../../pages/Setting/Setting'
-import Search from '../../pages/Search/Search'
-import Help from '../../pages/Help/Help'
-import Sidebar from '../Sidebar/Sidebar'
-import NotFound from '../../pages/NotFound/NotFound'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "../../pages/Login/Login";
+import Signup from "../../pages/Signup/Signup";
+import Home from "../../pages/Home/Home";
+import Follow from "../../pages/Follow/Follow";
+import Bookmark from "../../pages/Bookmark/Bookmark";
+import Profile from "../../pages/Profile/Profile";
+import School from "../../pages/School/School";
+import Setting from "../../pages/Setting/Setting";
+import Search from "../../pages/Search/Search";
+import Help from "../../pages/Help/Help";
+import Sidebar from "../Sidebar/Sidebar";
+import NotFound from "../../pages/NotFound/NotFound";
+import Write from "../../pages/Write/Write";
 
 const Router = () => {
   return (
@@ -18,18 +19,19 @@ const Router = () => {
       <div className="layout">
         <Sidebar />
         <Routes>
-          <Route path='/login' element={<Login />} />
-          <Route path='/signup' element={<Signup/>}/>
-          <Route path='/home' element={<Home />} />
-          <Route path='/follow' element={<Follow />}/>
-          <Route path='/bookmark' element={<Bookmark />}/>
-          <Route path='/profile/:username' element={<Profile />}/>
-          <Route path='/school' element={<School />}/>
-          <Route path='/setting' element={<Setting />}/>
-          <Route path='/search' element={<Search />}/>
-          <Route path='/help' element={<Help/>}/>
-          <Route path='/sidebar' element={<Sidebar/>}/>
-          <Route path='/*' element={<NotFound/>}/>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/follow" element={<Follow />} />
+          <Route path="/bookmark" element={<Bookmark />} />
+          <Route path="/profile/:username" element={<Profile />} />
+          <Route path="/school" element={<School />} />
+          <Route path="/setting" element={<Setting />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/help" element={<Help />} />
+          <Route path="/sidebar" element={<Sidebar />} />
+          <Route path="/*" element={<NotFound />} />
+          <Route path="/write" element={<Write/>}/>
         </Routes>
       </div>
     </BrowserRouter>
