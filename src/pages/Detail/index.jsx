c2504640e002d2a930d26d542385ff2197b90990
yejub <yejubeshbs22@gmail.com>
2024-11-29 21:53:10 +0900

    feat : Function complete
    기능 완료

--- src/pages/Detail/index.jsx
+++ src/pages/Detail/index.jsx
@@ -6,9 +6,9 @@ import { useParams } from "react-router-dom";
 import './style.css'
 import memoaAxios from "../../libs/axios/instance";
 import Tag from "../../components/Tag";
+import { string } from "prop-types";
 
 const Detail = () => {
-
   let params = useParams();
   params.id = params.id.substring(1);
 
@@ -23,77 +23,77 @@ const Detail = () => {
   });
 
   const getPostDetail = async () => {
-    try{
-      await memoaAxios.get(`/post/${params.id}`).then((res)=>setPostData(res.data))
-    }catch(err){
-      console.log(err)
+    try {
+      await memoaAxios.get(`/post/${params.id}`).then((res) => setPostData(res.data));
+    } catch (err) {
+      console.log(err);
     }
   }
-  
   const [which, setWhich] = useState(true);
 
   const what = () => {
-    if (which === true) {
-      setWhich(false);
-    } else if (which === false) {
-      setWhich(true);
-    }
+    setWhich(!which);
   };
 
-  useEffect(()=>{
-    getPostDetail()
-  }, [])
+  useEffect(() => {
+    getPostDetail();
+  }, []);
+
+  const renderContentWithImages = () => {
+    const contentParts = postData.content.split("✔").filter(Boolean);
+    return contentParts.map((part, index) => {
+      if (part.startsWith("★")) {
+        const imageUrl = part.slice(1);
+        return <img src={imageUrl} alt={`Content Image ${index}`} key={index} style={{ maxWidth: "100%" }} />;
+      }
+      return <span key={index}>{part}</span>;
+    });
+  }
 
   return (
     <div className="head-main">
       <Header />
       <div className="detail-post-container">
-      <div className="post-detail-container">
-        <div className="post-header">
-          <div className="post-detail-header">
-            <span style={{ fontSize: "1.6em", fontWeight: "500" }}>
-              {postData.title}
-            </span>
-            {which ? (
-              <MdBookmarkBorder
-                style={{
-                  width: "30px",
-                  height: "30px",
-                  color: "gray",
-                }}
-                onClick={what}
-              />
-            ) : (
-              <IoMdBookmark
-                style={{
-                  width: "30px",
-                  height: "30px",
-                }}
-                onClick={what}
-              />
-            )}
-          </div>
-          <div className="tag-and-author">
-            <div className="tag-container">
-              {postData.tags.map((item, index) => (
-                <Tag key={index} tagPrint={item} canActive={false} setDefault={true}/>
-              ))}
+        <div className="post-detail-container">
+          <div className="post-header">
+            <div className="post-detail-header">
+              <span style={{ fontSize: "1.6em", fontWeight: "500" }}>
+                {postData.title}
+              </span>
+              {which ? (
+                <MdBookmarkBorder
+                  style={{
+                    width: "30px",
+                    height: "30px",
+                    color: "gray",
+                  }}
+                  onClick={what}
+                />
+              ) : (
+                <IoMdBookmark
+                  style={{
+                    width: "30px",
+                    height: "30px",
+                  }}
+                  onClick={what}
+                />
+              )}
             </div>
-            <div className="author">작성자 : {postData.author}</div>
+            <div className="tag-and-author">
+              <div className="tag-container">
+                {postData.tags.map((item, index) => (
+                  <Tag key={index} tagPrint={item} canActive={false} setDefault={true} />
+                ))}
+              </div>
+              <div className="author">작성자 : {postData.author}</div>
+            </div>
+            <div className="post-line"></div>
           </div>
-          <div className="post-line"></div>
-        </div>
-        <div className="detail-main-post">
-          <img src={postData.images[0]}></img>
-        </div>
-        <div className="detail-post-content">
-          <span>{postData.content}</span>
-        </div>
-        <div className="detail-post-content">
-          <span>{postData.content}</span>
+          <div className="detail-post-content">
+            {renderContentWithImages()}
+          </div>
         </div>
       </div>
-    </div>
     </div>
   );
 };
